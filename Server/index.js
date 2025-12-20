2a65db79f8177398ead7061434c803eac3264d4f
Ducket191 <dangminhduc1912008@gmail.com>
2025-12-20 16:43:20 +0700

    update

--- Server/index.js
+++ Server/index.js
@@ -81,7 +81,7 @@ app.post("/alert", async (req,res) => {
 })
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
     // Send immediately once on server start
